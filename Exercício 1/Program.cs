a97a86004f58929507b26a8154b01e1826b6df51
ProjetosMariaisadora <100208644+ProjetosMariaisadora@users.noreply.github.com>
2023-05-16 12:08:28 -0300

    Add files via upload

---
+++ Exercício 1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercício_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Cao c = new Cao();
+       Gato g = new Gato();
+       Homem h = new Homem();
+
+       Console.WriteLine("O Cachorro emite o " + c.fala());
+       Console.WriteLine("O gato emite o " + g.fala());
+       Console.WriteLine("O Homem emite o " + h.fala());
+    }
+}
